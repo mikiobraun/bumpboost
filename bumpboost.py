5058fdfd0b94eee50380b0f7dd234761bcc23971
mikiobraun <mikiobraun@gmail.com>
2020-07-20 21:10:26 +0200

    Adding gitignore and some reformatting

--- bumpboost.py
+++ bumpboost.py
@@ -9,30 +9,43 @@ class BumpBoost:
         self.widths_candidates = width_candidates
 
     def fit(self, x, y):
+        # setting up all the result arrays
         dims = x.shape[1]
+
         y2 = y.dot(y)
         self.alpha = np.zeros(self.iterations)
         self.width = np.zeros(self.iterations)
         self.base = np.zeros((self.iterations, dims))
 
+        c = np.zeros(len(self.widths_candidates))
+        a = np.zeros(len(self.widths_candidates))
+
+        # start with the residual
         r = np.array(y)
         for n in range(self.iterations):
+            # Sample a good point based on the residual
             i = self.choose(r)
             xi = x[i,:]
-            c = np.zeros(len(self.widths_candidates))
-            a = np.zeros(len(self.widths_candidates))
+
+            # Try out all widths candidates for best fit
             for l, w in enumerate(self.widths_candidates):
                 k = self.kernfct(w, xi, x)
                 kr = k.dot(r)
                 kk = k.dot(k)
                 a[l] = kr / kk
                 c[l] = a[l] * kr
+
+            # pick best one
             j = np.argmax(c)
             self.alpha[n] = a[j]
             self.width[n] = self.widths_candidates[j]
             self.base[n, :] = xi
             yh = self.alpha[n] * self.kernfct(self.width[n], xi, x)
+
+            # update residual
             r -= yh
+
+            # Print some logs
             print(f'iter #{n} residual {r.dot(r) / y2:.3f}% last alpha {self.alpha[n]}')
 
     def predict(self, x):
@@ -64,7 +77,7 @@ if __name__ == '__main__':
     #from sklearn.svm import SVR
     #svr = SVR()
 
-    bb = BumpBoost(200, np.linspace(-2, 2))
+    bb = BumpBoost(100, np.linspace(-2, 2))
     bb.fit(x,y)
 
     print(bb.alpha)
